8fb95d403cb7489aafe832bd59d3112ca8e2d985
selimalahi <alahi.selim@gmail.com>
2022-11-10 21:03:59 +0600

    jwt update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -23,6 +24,12 @@ async function run() {
     const serviceCollection = client.db("onlineService").collection("services");
     const reviews = client.db("onlineService").collection("reviews");
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token })
+      console.log({token})
+    })
     app.get("/services", async (req, res) => {
       const query = {};
       limit = 3;
